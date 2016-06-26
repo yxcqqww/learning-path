69f6eeec46ed57f191a2a092195c1de172f130b1
leilangfeng2014 <leilangfeng2014@aliyun.com>
2016-06-26 12:23:40 +0800

    #

---
+++ apps/m.guanaihui.com/js/cuicianbing.js
@@ -0,0 +1,8 @@
+/*
+* @Author: LLF
+* @Date:   2016-06-26 12:20:49
+* @Last Modified by:   LLF
+* @Last Modified time: 2016-06-26 12:22:42
+*/
+
+'use strict';
---
+++ apps/m.guanaihui.com/jssrc/cuixianbing.js
@@ -0,0 +1,8 @@
+/*
+* @Author: LLF
+* @Date:   2016-06-26 12:22:59
+* @Last Modified by:   LLF
+* @Last Modified time: 2016-06-26 12:23:00
+*/
+
+'use strict';
